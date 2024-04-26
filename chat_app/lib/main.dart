85eb51b844f77ba2fd0aa5e14a28f2efbd338726
Yash pathak <assush698@gmail.com>
2024-04-26 23:33:52 +0530

    initial

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:chat_app/presentation/chat.dart';
+import 'package:chat_app/presentation/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Chat Bot',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
+        useMaterial3: true,
+      ),
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => const HomePage(),
+        ChatPage.routeName: (context) => const ChatPage(),
+      },
+    );
+  }
+}
